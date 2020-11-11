d1d95877b1dd38607e26c2a1b10f7d0d75563f2f
dlawrenncar <dlawren@ucar.edu>
2020-11-11 13:45:19 -0700

    Output formatting

--- mysci.py
+++ mysci.py
@@ -43,7 +43,16 @@ windchill = []
 for temp, windspeed in zip(data['tempout'],data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
 
+# Output comparison of data
+print(' Date    Time  Windchill Windchill Difference')
+print('               original  compute')
+print('------- ------ --------- --------- ----------')
+
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date, time, wc_orig, wc_comp in zip_data:
+    wc_diff = wc_comp - wc_orig
+    print(f'{date} {time:>6} {wc_orig:9.3f} {wc_comp:9.3f} {wc_diff:10.3f}')
 
 # DEBUG
-for windchill_data, windchill_comp in zip(data['windchill'],windchill):
-    print(f'{windchill_data:.5f} {windchill_comp:.5f} {windchill_data-windchill_comp:.5f}')
+#for windchill_data, windchill_comp in zip(data['windchill'],windchill):
+#    print(f'{windchill_data:.5f} {windchill_comp:.5f} {windchill_data-windchill_comp:.5f}')
