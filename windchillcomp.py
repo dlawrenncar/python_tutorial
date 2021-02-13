6738aea7741e7183e7793a89e7471a8abb7e6c63
David Lawrence <dlawren@ucar.edu>
2021-02-13 16:44:32 -0700

    update windchill to use function read

--- windchillcomp.py
+++ windchillcomp.py
@@ -1,31 +1,13 @@
+from readdata import read_data
+
 # Column naems and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7, 'windchill':12}
 
 # Datatypes for each column
 types = {'tempout': float, 'windspeed':float, 'windchill':float}
 
-# Initialize my data variable
-data = {}
-for column in columns:
-    data[column] = []
-
-# Read the data fiel
-filename = "data/wxobs20180821.txt"
-
-with open(filename, 'r') as datafile:
-
-    # read the first three lines (header)
-    for _ in range(3):
-        datafile.readline()
-
-    # Read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+# Read data from file
+data = read_data(columns, types=types)
 
 # Compute windchill temperature
 def compute_windchill(t, v):
