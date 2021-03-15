5fd2c469df9d9dd9d7aed26c564309b174c596fd
David Lawrence <dlawren@ucar.edu>
2021-03-15 12:39:56 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptool import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Dave Lawrence",
+    author_email="dlawren@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
